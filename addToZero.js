1df0c3575fb7069256cb70634675e5ab85cd2117
MadsenWalker <madsenwalker@gmail.com>
2023-03-06 10:54:17 -0700

    morning

---
+++ addToZero.js
@@ -0,0 +1,18 @@
+// Starting array
+let array = [28, 43, -12, 30, 4, 0, 12]
+
+// Write your solution below:
+
+let addToZero= false
+
+for(let i = 0; i < array.length; i++){
+    for(let j = 1; j < array.length; j++){
+        if(i !== j){
+            if(array[i] + array[j] === 0){
+                addToZero = true
+            }
+            
+        }
+       
+} }
+console.log(addToZero)
